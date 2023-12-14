03a42011bb86bd76c369a1241057765a677d51fe
FooQoo <horseshoes.buzz.17@gmail.com>
2023-12-14 22:29:45 +0900

    不要なリンクを削除

--- src/components/atoms/global-menu/user/GlobalUserMenu.tsx
+++ src/components/atoms/global-menu/user/GlobalUserMenu.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { User } from '@/types/user';
