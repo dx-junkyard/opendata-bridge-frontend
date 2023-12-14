35137e546d0d9cd97d1f8bb025199cc4a6148897
FooQoo <horseshoes.buzz.17@gmail.com>
2023-12-14 21:40:29 +0900

    Heroを実装

---
+++ src/components/molecules/hero/Hero.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Hero } from '@/components/molecules/hero/Hero';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'OpendataBridge/Molecules/Hero',
+  component: Hero,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof Hero>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/components/molecules/hero/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Hero = () => {
+  return (
+    <div className="w-full h-[323px] text-black bg-blue-100 text-center flex flex-col justify-center space-y-5">
+      <h1 className="text-bold text-6xl">Opendata Bridge</h1>
+      <h2 className="text-4xl">利用目的で探せるオープンデータ検索サービス</h2>
+    </div>
+  );
+};
