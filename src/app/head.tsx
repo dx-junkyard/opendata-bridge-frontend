1d21f547a7a8024ccddf3490137a3819cf5b35ce
FooQoo <horseshoes.buzz.17@gmail.com>
2023-12-23 21:30:20 +0900

    favicon追加

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+const Head = () => {
+  return (
+    <>
+      <title>OpenData Bridge</title>
+      <meta
+        name="description"
+        content={'市民とオープンデータをつなぐプラットフォーム'}
+      />
+      <link rel="icon" href={'/favicon.ico'} />
+    </>
+  );
+};
+
+export default Head;
