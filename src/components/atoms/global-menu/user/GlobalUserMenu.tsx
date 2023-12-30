45668f15182ba827103af1fe24413bc40f5e3c3a
FooQoo <horseshoes.buzz.17@gmail.com>
2023-12-30 22:46:14 +0900

    fix: ヘッダのボーダーの修正

--- src/components/atoms/global-menu/user/GlobalUserMenu.tsx
+++ src/components/atoms/global-menu/user/GlobalUserMenu.tsx
@@ -9,7 +9,7 @@ interface GlobalUserMenuProps {
 
 export const GlobalUserMenu = ({ user }: GlobalUserMenuProps) => {
   return (
-    <div className="w-full md:min-w-[110px] min-h-[80px] bg-white flex justify-center items-center text-blue-700 border-b-4 border-blue-700">
+    <div className="w-full md:min-w-[110px] min-h-[80px] bg-white flex justify-center items-center text-blue-700 border-b md:border-b-4 md:border-blue-700">
       <FontAwesomeIcon icon={faUser} className="flex-none mx-1 h-[30px]" />
       <h2 className="font-bold flex-grow text-center">
         {user?.name || 'ゲスト'}
