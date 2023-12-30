eca65a6b62ff7d0fdee4272bfb0db3c9631c677d
FooQoo <horseshoes.buzz.17@gmail.com>
2023-12-31 00:54:28 +0900

    feat: 詳細画面のレスポンシブ対応

--- src/components/templates/detail-project/DetailProject.tsx
+++ src/components/templates/detail-project/DetailProject.tsx
@@ -30,18 +30,18 @@ export const DetailProject = ({
 
   return (
     <article>
-      <div className="bg-white text-black px-[220px] py-[50px] flex flex-col space-y-2">
-        <h1 className="text-4xl">プロジェクト詳細情報</h1>
+      <div className="bg-white text-black px-[10px] md:px-[220px] py-[50px] flex flex-col space-y-2">
+        <h1 className="text-2xl md:text-4xl">プロジェクト詳細情報</h1>
         <ProjectCard project={project} />
         <ProjectTags tags={project.tags} />
       </div>
-      <div className="bg-white text-black px-[220px] py-[50px] flex flex-col space-y-2">
+      <div className="bg-white text-black px-[10px] md:px-[220px] py-[50px] flex flex-col space-y-2">
         <h2 className="text-xl">変換元のオープンデータのリンク一覧</h2>
         <div>{datasetList}</div>
       </div>
-      <div className="w-full text-black bg-blue-100 grid grid-cols-2 gap-24 px-[320px] py-[50px]">
+      <div className="w-full text-black bg-blue-100 grid grid-cols-1 md:grid-cols-2 gap-24 px-[10px] md:px-[320px] py-[50px]">
         <ActionCard
-          title="データ整形レシピを実行する"
+          title="データ整形レシピの実行"
           description="作成済みのデータ整形レシピを使って、データ整形を実行します"
           url={`/project/${project.id}/use-recipe/`}
           thumbnail="/dummy1_1.png"
@@ -49,7 +49,7 @@ export const DetailProject = ({
         />
         {isLogin ? (
           <ActionCard
-            title="データ整形レシピを開発する"
+            title="データ整形レシピの開発"
             description="AIを使ってデータ整形用のレシピを開発します"
             url={`/project/${project.id}/develop-recipe/`}
             thumbnail="/dummy1_1.png"
@@ -57,7 +57,7 @@ export const DetailProject = ({
           />
         ) : (
           <ActionCard
-            title="データ整形レシピを開発する"
+            title="データ整形レシピの開発"
             description="データ整形レシピの開発はログインユーザのみ利用できます"
             thumbnail="/dummy1_1.png"
             buttonLabel="レシピを開発する"
