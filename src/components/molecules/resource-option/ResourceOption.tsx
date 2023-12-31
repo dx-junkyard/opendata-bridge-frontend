17c182fbd205125d3ce7248ba96a6b38cfabe6a5
FooQoo <horseshoes.buzz.17@gmail.com>
2023-12-31 09:48:53 +0900

    fix: 開発画面のフォーマット崩れを修正する

--- src/components/molecules/resource-option/ResourceOption.tsx
+++ src/components/molecules/resource-option/ResourceOption.tsx
@@ -12,7 +12,7 @@ export const ResourceOption = ({ resource, onDelete }: ResourceOptionProps) => {
   return (
     <div className="border-gray-300 border-b w-full h-[50px] bg-white text-black flex justify-center items-center">
       <div className="text-center">
-        <FontAwesomeIcon icon={faCircle} />
+        <FontAwesomeIcon icon={faCircle} width={16} height={16} />
       </div>
       <div className="m-[16px] w-[90%]">
         <p className="text-sm text-blue-800 font-bold">{resource.name}</p>
@@ -21,7 +21,7 @@ export const ResourceOption = ({ resource, onDelete }: ResourceOptionProps) => {
         className="text-center flex justify-center items-center space-x-2"
         onClick={onDelete}
       >
-        <FontAwesomeIcon icon={faTrash} />
+        <FontAwesomeIcon icon={faTrash} width={16} height={16} />
       </button>
     </div>
   );
