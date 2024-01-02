86a653286a006fafefcbdef5f651902cd1554b7a
FooQoo <horseshoes.buzz.17@gmail.com>
2024-01-02 21:10:12 +0900

    style: ローディング画面のstyleを修正

--- src/components/molecules/project/loading-project-card/LoadingProjectCard.tsx
+++ src/components/molecules/project/loading-project-card/LoadingProjectCard.tsx
@@ -1,6 +1,6 @@
 export const LoadingProjectCard = () => {
   return (
-    <div className="grid border-gray-300 border-b w-full md:h-[187px] grid-cols-2 place-content-center bg-white py-[30px]">
+    <div className="grid border-gray-300 border-b w-full grid-cols-2 place-content-center bg-white py-[30px]">
       <div className="flex justify-center items-center space-y-8 w-full h-full">
         <div className="text-sm w-[70%] md:w-[80%] font-bold animate-pulse bg-slate-300 h-[20px] mx-[40px] rounded" />
       </div>
