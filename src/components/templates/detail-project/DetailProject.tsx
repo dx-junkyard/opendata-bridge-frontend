bd023792803bbfee0cc6e38f2d6c5dff0031a057
FooQoo <horseshoes.buzz.17@gmail.com>
2024-01-03 17:53:13 +0900

    style: レスポンシブを調整

--- src/components/templates/detail-project/DetailProject.tsx
+++ src/components/templates/detail-project/DetailProject.tsx
@@ -19,40 +19,42 @@ export const DetailProject = ({
   const script = project.recipes[0]?.script || '';
 
   return (
-    <article>
-      <div className="bg-white text-black px-[10px] md:px-[220px] py-[50px] flex flex-col space-y-2">
+    <article className="w-full flex flex-col justify-center items-center">
+      <div className="w-full bg-white text-black px-[10px] py-[50px] flex flex-col space-y-2">
         <h1 className="text-2xl md:text-4xl">プロジェクト詳細情報</h1>
         <ProjectCard project={project} />
         <ProjectTags tags={project.tags} />
       </div>
-      <div className="bg-white text-black px-[10px] md:px-[220px] py-[50px] flex flex-col space-y-2">
+      <div className="w-full bg-white text-black px-[10px] py-[50px] flex flex-col space-y-2">
         <h2 className="text-xl">変換元のオープンデータのリンク一覧</h2>
         <div>
           <DatasetLinkCardList datasetList={project.resources} />
         </div>
       </div>
       {script && (
-        <div className="bg-white text-black px-[10px] md:px-[220px] pt-[50px] pb-[100px] flex flex-col space-y-2 ">
+        <div className="w-full bg-white text-black px-[10px] pt-[50px] pb-[100px] flex flex-col space-y-2 ">
           <h2 className="text-xl">変換用のPythonコード</h2>
           <CodeEditor code={script} language="python" />
         </div>
       )}
       {isLogin && (
-        <div className="w-full text-black bg-blue-100 grid grid-cols-1 md:grid-cols-2 gap-24 px-[10px] md:px-[320px] py-[50px]">
-          <ActionCard
-            title="データ整形レシピの実行"
-            description="作成済みのデータ整形レシピを使って、データ整形を実行します"
-            url={`/project/${project.id}/use-recipe/`}
-            thumbnails="/dummy1_1.png"
-            buttonLabel="レシピを実行する"
-          />
-          <ActionCard
-            title="データ整形レシピの開発"
-            description="AIを使ってデータ整形用のレシピを開発します"
-            url={`/project/${project.id}/develop-recipe/`}
-            thumbnails="/dummy1_1.png"
-            buttonLabel="レシピを開発する"
-          />
+        <div className="w-screen text-black bg-blue-100 flex items-center justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-24 px-[10px] py-[50px] max-w-[1000px]">
+            <ActionCard
+              title="データ整形レシピの実行"
+              description="作成済みのデータ整形レシピを使って、データ整形を実行します"
+              url={`/project/${project.id}/use-recipe/`}
+              thumbnails="/dummy1_1.png"
+              buttonLabel="レシピを実行する"
+            />
+            <ActionCard
+              title="データ整形レシピの開発"
+              description="AIを使ってデータ整形用のレシピを開発します"
+              url={`/project/${project.id}/develop-recipe/`}
+              thumbnails="/dummy1_1.png"
+              buttonLabel="レシピを開発する"
+            />
+          </div>
         </div>
       )}
     </article>
