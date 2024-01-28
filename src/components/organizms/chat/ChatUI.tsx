ba08efac864f0fe474e554c93b14cbc30752a39b
fooqoo <horseshoes.buzz.17@gmail.com>
2024-01-28 19:48:45 +0900

    feat: チャットの停止機能

--- src/components/organizms/chat/ChatUI.tsx
+++ src/components/organizms/chat/ChatUI.tsx
@@ -19,6 +19,7 @@ export const ChatUI: FC<ChatUIProps> = ({ project }) => {
     removeFile,
     messages,
     isChatting,
+    cancelChat,
   } = useInputPrompt(project.recipes[0]?.prompt || '');
 
   return (
@@ -45,6 +46,7 @@ export const ChatUI: FC<ChatUIProps> = ({ project }) => {
             updatePrompt={updatePrompt}
             actionUsePrompt={actionUsePrompt}
             isChatting={isChatting}
+            cancelChat={cancelChat}
           />
         </div>
       </div>
